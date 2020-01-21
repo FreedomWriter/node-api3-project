78a62433de3f5b32e7f931bf806ad48d9f6394c2
FreedomWriter <natalienetworks@gmail.com>
2020-01-20 19:32:49 -0600

    validate postID working

--- posts/postRouter.js
+++ posts/postRouter.js
@@ -1,27 +1,106 @@
-const express = require('express');
+const express = require("express");
+
+const db = require("./postDb");
 
 const router = express.Router();
 
-router.get('/', (req, res) => {
+router.get("/", (req, res) => {
+  // do your magic!
+  db.get()
+    .then(posts => res.status(200).json({ success: true, posts }))
+    .catch(err =>
+      res
+        .status(500)
+        .json({ success: false, message: "Unable to retrieve posts." })
+    );
+});
+
+router.get("/:id", validatePostId, (req, res) => {
+  // do your magic!
+  const { id } = req.params;
+  db.getById(id)
+    .then(post => res.status(200).json({ success: true, post }))
+    .catch(err =>
+      res.status(500).json({ success: false, message: "Unable to get post" })
+    );
+});
+
+router.delete("/:id", validatePostId, (req, res) => {
   // do your magic!
 });
 
-router.get('/:id', (req, res) => {
+router.put("/:id", validatePostId, validatePost, (req, res) => {
   // do your magic!
+  db.update(req.params.id, req.body)
+    .then(post => res.status(200).json({ success: true, post }))
+    .catch(err => {
+      console.log(err);
+      req
+        .status(500)
+        .json({ success: false, message: "Unable to update post" });
+    });
 });
 
-router.delete('/:id', (req, res) => {
-  // do your magic!
-});
-
-router.put('/:id', (req, res) => {
-  // do your magic!
+router.post("/", validatePost("text"), (req, res) => {
+  console.log(`router.post: `, req.body);
+  db.insert(req.body)
+    .then(post => res.status(201).json({ success: true, post }))
+    .catch(err => res.status(500).json({ success: false, message: err }));
 });
 
 // custom middleware
 
 function validatePostId(req, res, next) {
   // do your magic!
+  const { id } = req.params;
+  db.getById(id)
+    .then(post => {
+      console.log(post);
+      if (typeof post === "object") {
+        req.post = post;
+        next();
+      } else {
+        res.status(404).json({ message: "Invalid post ID" });
+      }
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json({ message: "Invalid post ID", err });
+    });
 }
+
+function validatePost(prop) {
+  console.log(prop);
+  return function(req, res, next) {
+    console.log(req.body);
+    if (req.body) {
+      if (req.body[prop]) {
+        next();
+      } else {
+        res
+          .status(400)
+          .json({ success: false, message: `Missing required ${prop} field.` });
+      }
+    } else {
+      res.status(400).json({ success: false, message: "Missing post data." });
+    }
+  };
+}
+
+// function validatePost(req, res) {
+//   // const body = req.body;
+//   console.log(`validate: `, req.body);
+//   if (req.body) {
+//     if (req.body.text) {
+//       next();
+//     } else {
+//       res
+//         .status(400)
+//         .json({ success: false, message: "Missing required text field." });
+//     }
+//   } else {
+//     res.status(400).json({ success: false, message: "Missing post data." });
+//   }
+// }
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,13 +1,24 @@
-const express = require('express');
+const express = require("express");
 
 const server = express();
 
-server.get('/', (req, res) => {
+const postsRouter = require("./posts/postRouter");
+
+server.use(logger);
+
+server.use("/api/posts", postsRouter);
+
+server.get("/", (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`);
 });
 
 //custom middleware
 
-function logger(req, res, next) {}
+function logger(req, res, next) {
+  const { method, originalUrl } = req;
+  const date = new Date().toISOString();
+  console.log(`${method} on ${originalUrl} at ${date}`);
+  next();
+}
 
 module.exports = server;
